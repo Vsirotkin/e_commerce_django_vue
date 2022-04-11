fb38809872652ffe31b8ab6ea68a0ff16a5c2246
Viktor <vsirotkin15@gmail.com>
2022-04-11 10:05:27 +0300

    navbar,  login, cart, footer

--- djackets_vue/src/App.vue
+++ djackets_vue/src/App.vue
@@ -1,10 +1,8 @@
 <template>
-<div id="wrapper">
-
+  <div id="wrapper">
     <nav class="navbar is-dark">
       <div class="navbar-brand">
-        <router-link to="/" class="navbar-item"><strong>Djackets</strong>
-        </router-link>
+        <router-link to="/" class="navbar-item"><strong>Djackets</strong></router-link>
 
         <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
           <span aria-hidden="true"></span>
@@ -12,29 +10,31 @@
           <span aria-hidden="true"></span>
         </a>
       </div>
-      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
-          <div class="navbar-start">
-            <div class="navbar-item">
-              <form method="get" action="/search">
-                <div class="field has-addons">
-                  <div class="control">
-                    <input type="text" class="input" placeholder="What are you looking for?" name="query">
-                  </div>
 
-                  <div class="control">
-                    <button class="button is-success">
-                        <span class="icon">
-                        <i class="fas fa-search"></i>
-                        </span>
-                    </button>
-                  </div>
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <form method="get" action="/search">
+              <div class="field has-addons">
+                <div class="control">
+                  <input type="text" class="input" placeholder="What are you looking for?" name="query">
                 </div>
-              </form>
-            </div>
+
+                <div class="control">
+                  <button class="button is-success">
+                      <span class="icon">
+                      <i class="fas fa-search"></i>
+                      </span>
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
-          <div class="navbar-end">
-            <router-link to="/summer" class="navbar-item">Summer</router-link>
-            <router-link to="/winter" class="navbar-item">Winter</router-link>
+        </div>
+
+        <div class="navbar-end">
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+          <router-link to="/winter" class="navbar-item">Winter</router-link>
 
           <div class="navbar-item">
             <div class="buttons">
@@ -54,10 +54,20 @@
           </div>
         </div>
       </div>
-  </nav>
-  <router-view/>
-</div>
+    </nav>
 
+    <div class="is-loading-bar has-text-centered" v-bind:class="{'is-loading': $store.state.isLoading }">
+      <div class="lds-dual-ring"></div>
+    </div>
+
+    <section class="section">
+      <router-view/>
+    </section>
+
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2022</p>
+    </footer>
+  </div>
 </template>
 
 <style lang="scss">
