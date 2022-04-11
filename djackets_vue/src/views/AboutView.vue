f293d45ed82ca53acd9b1cd7dbb8aea031ae4ff1
Viktor <vsirotkin15@gmail.com>
2022-04-11 10:10:15 +0300

    changed config of home.vue

--- djackets_vue/src/views/AboutView.vue
+++ djackets_vue/src/views/AboutView.vue
@@ -1,5 +1,26 @@
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="home">
+    <section class="hero is-medium is-dark mb-6">
+        <div class="hero-body has-text-centered">
+            <p class="title mb-6">
+                Welcome to Djacket
+            </p>
+            <p class="subtitle">
+                The best jacket store online
+            </p>
+        </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+          <h2 class="is-size-2 has-text-centered">Latest products</h2>
+      </div>
+
+      <ProductBox 
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+        v-bind:product="product" />
+    </div>
   </div>
 </template>
